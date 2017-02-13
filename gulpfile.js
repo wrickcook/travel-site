67588f7bf81ddf231f222b7a0b8fc140ec239047
Rick <rickcook22@gmail.com>
2017-02-12 16:27:00 -0800

    organize css

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 autoprefixer = require('autoprefixer'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 
 gulp.task('default', function(){
@@ -24,14 +25,33 @@ return gulp.src('./app/assets/styles/styles.css')
 });
 
 
+
+
+
+
 gulp.task('watch', function() {
 
+browserSync.init({
+notify: false,
+server: {
+baseDir: "app"
+}
+});
+
 watch('./app/index.html', function(){
-gulp.start('html');
+browserSync.reload();
 });
 
 watch('./app/assets/styles/**/*.css', function(){
-gulp.start('styles');
+gulp.start('cssInject');
 });
 
+});
+
+
+
+
+gulp.task('cssInject', ['styles'],function(){
+return gulp.src('./app/temp/styles/styles.css')
+.pipe(browserSync.stream());
 });
